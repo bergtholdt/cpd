6671fb4f4ba30e7b005ea1214acfa6f3672ba82b
Martin Bergtholdt <martin.bergtholdt@philips.com>
2018-07-06 15:01:51 +0200

    ENH: activated OpenMP, full result output

--- PythonWrapper/_cpd.pyx
+++ PythonWrapper/_cpd.pyx
@@ -4,7 +4,8 @@ from libc.string cimport memcpy
 from libcpp.string cimport string
 from eigency.core cimport ndarray_copy, ndarray, Map
 #from _cpd cimport Transform, Matrix, Result, Value, get_json, Rigid, Affine, to_json
-from _cpd cimport Transform, Matrix, Result, Rigid, Affine, Value, to_json, get_json
+from _cpd cimport Transform, Matrix, Result, Rigid, RigidResult, AffineResult, Affine, Value, to_json, get_json
+import json
 
 cdef class CPD:
     cdef np.ndarray _fixed
@@ -19,19 +20,21 @@ cdef class CPD:
 
     def rigid(self, reflections=False, scale=False):
         cdef Rigid rigid = Rigid()
-        print(1)
         rigid.scale(scale)
-        print(2)
         rigid.reflections(reflections)
-        print(3)
-        self._result = rigid.run(Map[Matrix](self._fixed),
+        cdef RigidResult res = rigid.run(Map[Matrix](self._fixed),
                                  Map[Matrix](self._moving))
-        print(4)
+        cdef Value val = to_json(res)
+        cdef string result = get_json(val)
+        return json.loads(result)
 
     def affine(self, linked=True):
         cdef Affine affine = Affine()
         affine.linked(linked)
-        self._result = affine.run(Map[Matrix](self._fixed), Map[Matrix](self._moving))
+        cdef AffineResult res = affine.run(Map[Matrix](self._fixed), Map[Matrix](self._moving))
+        cdef Value val = to_json(res)
+        cdef string result = get_json(val)
+        return json.loads(result)
 
     property fixed:
         def __get__(self):
@@ -46,12 +49,6 @@ cdef class CPD:
 
         def __set__(self, np.ndarray[np.float64_t, ndim=2] moving):
             self._moving = moving
-
-    property result:
-        def __get__(self):
-            cdef Value val = to_json(self._result)
-            cdef string res = get_json(val)
-            return res
 
         # def __set__(self, args):
         #     fi = self._sys_geo_cpd
